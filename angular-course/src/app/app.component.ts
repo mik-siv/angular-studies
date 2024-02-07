ba098afbf8266d84daf782672c4a9b3fff1478fe
mikhail.sivinskikh <mikhail.sivinskikh@gmail.com>
2024-02-07 21:16:31 +0200

    Update app.component functionality and styles
    The app.component's styles and functionality have been updated. Specifically, some class styles have been added to the CSS and functions for title click and key up events have been added to the TypeScript file. The HTML file has also been cleaned up significantly, removing a large amount of placeholder and unused code.

--- angular-course/src/app/app.component.ts
+++ angular-course/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +9,13 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-course';
+  title = 'Angular Course';
+
+  onTitleClick() {
+    alert('Title clicked')
+  }
+
+  onKeyUp(newTitleInput: string) {
+    this.title = newTitleInput
+  }
 }
