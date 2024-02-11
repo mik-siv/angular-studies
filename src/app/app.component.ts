dfcacbc0c8bc84cd3c6af8425561d3c542bc4f69
mikhail.sivinskikh <mikhail.sivinskikh@gmail.com>
2024-02-11 20:40:24 +0200

    Add CourseCardComponent to app module and update AppComponent
    This update implements the CourseCardComponent into the app module and expands the AppComponent. AppComponent now contains functionality for course selection, as well as the ability to track a course. Additionally, app.component.html was updated to contain a visual representation of these changes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {COURSES} from '../db-data';
+import {Course} from "./model/course";
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,15 @@ import {COURSES} from '../db-data';
 })
 export class AppComponent {
 
+  courses: Course[] = [...COURSES];
+  title: string = this.courses[0].description
+  startDate: Date = new Date('2000-01-01')
 
+  onCourseSelected(course: Course) {
+    console.log(course)
+  }
 
+  trackCourse(course: Course) {
+    return course.id
+  }
 }
